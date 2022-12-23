5d7fa3e3da3bf9c1f365c092e03633095d4ce6fc
ciftci-mehmet <mciftci@maritimetrainer.com>
2022-12-23 16:33:18 +0300

    json error responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -43,4 +45,42 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var synTaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &synTaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", synTaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		// json value is wrong type for target destination
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		// empty body
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		// if non nil pointer to decode panic
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+
+		}
+
+	}
+	return nil
 }
