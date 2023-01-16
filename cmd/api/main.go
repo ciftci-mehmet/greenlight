180b4dc27c6aaf2524f438f0401d2a17f3836b82
ciftci-mehmet <mciftci@maritimetrainer.com>
2023-01-16 15:21:31 +0300

    simple cors requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -40,6 +41,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -81,6 +85,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", mailTrapUser, "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", mailTrapPassword, "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@test.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space seperated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
