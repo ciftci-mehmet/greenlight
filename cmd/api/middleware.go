ed5c5f658d6369de6c36f680786a066f96c84216
ciftci-mehmet <mciftci@maritimetrainer.com>
2023-01-16 15:45:35 +0300

    preflight cors requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -194,12 +194,24 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
 
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		origin := r.Header.Get("Origin")
 
 		if origin != "" {
 			for i := range app.config.cors.trustedOrigins {
 				if origin == app.config.cors.trustedOrigins[i] {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// check if preflight request
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					break
 				}
 			}
